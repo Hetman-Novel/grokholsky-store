0f37c1645653eb2809f258d93d22fb31b2534a99
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-05-08 19:29:17 +0300

    Add files via upload

--- files/js/init-map.js
+++ files/js/init-map.js
@@ -4,17 +4,6 @@ function initMap() {
       lng: -122.419273
    };
 
-   // Mobile menu
-   var mobile_map = new google.maps.Map(document.getElementById('mobile-map'), {
-      zoom: 16,
-      center: uluru
-   });
-   var marker = new google.maps.Marker({
-      position: uluru,
-      map: mobile_map
-   });
-
-   // Footer
    var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: uluru
